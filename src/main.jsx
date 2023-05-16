1d1a10e24cb9b9500fabdd0755e82414c40c6338
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2023-05-17 00:10:41 +0200

    Feat: name of main component changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MyCoinWallet from "./MyCoinWallet.jsx";
+import App from "./McwApp.jsx";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MyCoinWallet />
+    <App />
   </React.StrictMode>
 );
