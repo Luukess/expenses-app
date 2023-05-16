d5710f3f799cff6523c1194a504902780ca9fc0c
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2023-05-17 00:03:26 +0200

    Feat: installed libs, changed favicon

---
+++ src/MyCoinWallet.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+function App() {
+
+  return (
+    <>
+      <Box sx={{ height: '100vh', width: '100vw', backgroundColor: 'red' }}>
+        
+      </Box>
+    </>
+  )
+}
+
+export default App;
