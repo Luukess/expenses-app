03ca5d71eec5a904163bf3c671017d8bf906975d
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2023-05-19 01:03:42 +0200

    Feat: created custom sidebar

--- src/components/main-layout/MainLayout.jsx
+++ src/components/main-layout/MainLayout.jsx
@@ -1,18 +1,77 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, { useContext, useEffect, useState } from "react";
+import { Box, IconButton, List, ListItemButton, useTheme } from "@mui/material";
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import WalletIcon from '@mui/icons-material/Wallet';
+import BookIcon from '@mui/icons-material/Book';
+import PaidIcon from '@mui/icons-material/Paid';
+import ShowChartIcon from '@mui/icons-material/ShowChart';
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
 import Header from "../header/Header";
+import { AuthContext } from "../../contexts/auth-context/authContext";
 import { sxStylesMainLayout } from "./mainLoyout.style";
 
-const MainLayout = ({children}) => {
+const MainLayout = ({ children }) => {
+
+    const [hideNav, setHideNav] = useState(JSON.parse(localStorage.getItem('hiddenNav')) ? true : false);
+
+    const authValueContext = useContext(AuthContext);
+
+    const theme = useTheme();
+
+    const handleHideNav = () => {
+        setHideNav(!hideNav);
+    };
+
+    useEffect(() => {
+        localStorage.setItem('hiddenNav', JSON.stringify(hideNav));
+    }, [hideNav])
 
     return (
         <>
+            <Header />
             <Box sx={sxStylesMainLayout.mainLayout}>
-                <Box sx={sxStylesMainLayout.navSidebar}>
-                    sadadadadadadadad
-                </Box>
+                {!authValueContext.isAuth &&
+                    <Box sx={!hideNav ? sxStylesMainLayout.navSidebar : { ...sxStylesMainLayout.navSidebar, width: '60px' }} >
+                        <Box sx={!hideNav ? sxStylesMainLayout.navButtonContainer : { ...sxStylesMainLayout.navButtonContainer, justifyContent: 'center' }}>
+                            <IconButton onClick={() => handleHideNav()}>
+                                <KeyboardDoubleArrowLeftIcon sx={hideNav ? { ...sxStylesMainLayout.navButton, transform: 'rotate(180deg)' } : sxStylesMainLayout.navButton} />
+                            </IconButton>
+                        </Box>
+                        <List component="nav" aria-labelledby="nested-list-subheader">
+                            <ListItemButton sx={sxStylesMainLayout.listItemButton}>
+                                <DashboardIcon sx={sxStylesMainLayout.listItemIcon} />
+                                <Box sx={!hideNav ? sxStylesMainLayout.navLink : { ...sxStylesMainLayout.navLink, opacity: 0 }} component='span'>
+                                    {'Podsumowanie'}
+                                </Box>
+                            </ListItemButton>
+                            <ListItemButton sx={sxStylesMainLayout.listItemButton}>
+                                <ShowChartIcon sx={sxStylesMainLayout.listItemIcon} />
+                                <Box sx={!hideNav ? sxStylesMainLayout.navLink : { ...sxStylesMainLayout.navLink, opacity: 0 }} component='span'>
+                                    {'Finanse'}
+                                </Box>
+                            </ListItemButton>
+                            <ListItemButton sx={sxStylesMainLayout.listItemButton}>
+                                <WalletIcon sx={sxStylesMainLayout.listItemIcon} />
+                                <Box sx={!hideNav ? sxStylesMainLayout.navLink : { ...sxStylesMainLayout.navLink, opacity: 0 }} component='span'>
+                                    {'Portfele'}
+                                </Box>
+                            </ListItemButton>
+                            <ListItemButton sx={sxStylesMainLayout.listItemButton}>
+                                <BookIcon sx={sxStylesMainLayout.listItemIcon} />
+                                <Box sx={!hideNav ? sxStylesMainLayout.navLink : { ...sxStylesMainLayout.navLink, opacity: 0 }} component='span'>
+                                    {'Planer'}
+                                </Box>
+                            </ListItemButton>
+                            <ListItemButton sx={sxStylesMainLayout.listItemButton}>
+                                <PaidIcon sx={sxStylesMainLayout.listItemIcon} />
+                                <Box sx={!hideNav ? sxStylesMainLayout.navLink : { ...sxStylesMainLayout.navLink, opacity: 0 }} component='span'>
+                                    {'Opłaty'}
+                                </Box>
+                            </ListItemButton>
+                        </List>
+                    </Box>
+                }
                 <Box component='main' sx={sxStylesMainLayout.childrenContainer}>
-                    <Header />
                     {children}
                 </Box>
             </Box>
--- src/components/main-layout/mainLoyout.style.js
+++ src/components/main-layout/mainLoyout.style.js
@@ -1,7 +1,6 @@
 export const sxStylesMainLayout = {
     mainLayout: {
-        width: '100vw',
-        height: '100vh',
+        height: 'calc(100vh - 64px)',
         display: 'flex',
         flexDirection: 'row',
     },
@@ -11,15 +10,41 @@ export const sxStylesMainLayout = {
             sm: 'none',
             md: 'block',
         },
-        width: '100px',
-        backgroundColor: 'red',
-        height: '100vh',
-        padding: '10px',
-        overflowWrap: 'break-word'
+        width: '225px',
+        height: 'calc(100vh - 64px)',
+        overflowWrap: 'break-word',
+        overflowY: 'auto',
+        transition: 'width 0.5s',
+        overflowX: 'hidden'
     },
     childrenContainer: {
         width: '100%',
         backgroundColor: 'pink',
         overflowY: 'auto'
+    },
+    listItemButton: {
+        fontSize: '17px',
+        height: '42px',
+        margin: '4px 0px',
+        height: '50px'
+    },
+    listItemIcon: {
+        marginRight: '10px',
+    },
+    navButton: {
+        fontSize: '26px',
+        transition: 'rotate 0.5s'
+    },
+    navLink: {
+        opacity: 1,
+        transition: 'opacity 0.5s'
+    },
+    navButtonContainer: {
+        padding: '0px 10px !important',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        height: '50px',
+        marginTop: '8px'
     }
 }
