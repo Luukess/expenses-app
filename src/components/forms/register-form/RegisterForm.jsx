4c5947d0bbb64457a4ca0895187e720895a7074f
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2023-05-23 08:50:23 +0200

    Feat: added transtaltions in register form

--- src/components/forms/register-form/RegisterForm.jsx
+++ src/components/forms/register-form/RegisterForm.jsx
@@ -4,11 +4,14 @@ import CustomInput from "../../custom-input/CustomInput";
 import { sxStylesRegisterForm } from "./registerForm.style";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const RegisterForm = () => {
 
     const theme = useTheme();
     const navigate = useNavigate();
+    const { t } = useTranslation();
+
     const [passwordValue, setPasswordValue] = useState('');
     const [repeatPasswordValue, setRepeatPasswordValue] = useState('');
 
@@ -30,11 +33,11 @@ const RegisterForm = () => {
         <>
             <Box component='form' onSubmit={handleSubmit(onSubmit)}>
                 <Box sx={sxStylesRegisterForm.boxFoemElement}>
-                    <CustomInput sx={sxStylesRegisterForm.formInputElement} type='text' placeholder='Login' {...register('userName', {
-                        required: 'Field is required',
+                    <CustomInput sx={sxStylesRegisterForm.formInputElement} type='text' placeholder={`${t('login-placeholder')}`} {...register('userName', {
+                        required: `${t('form-required-message')}`,
                         minLength: {
                             value: 4,
-                            message: 'Login is to short. Min 4 chars'
+                            message: `${t('form-login-placeholder-message')}`
                         }
                     })} />
                     <Typography fontSize={14} ml={1} color={theme.palette.error.main} >
@@ -42,11 +45,11 @@ const RegisterForm = () => {
                     </Typography>
                 </Box>
                 <Box sx={sxStylesRegisterForm.boxFoemElement}>
-                    <CustomInput sx={sxStylesRegisterForm.formInputElement} type='text' placeholder='Email' {...register('email', {
-                        required: 'Filed is required',
+                    <CustomInput sx={sxStylesRegisterForm.formInputElement} type='text' placeholder={`${t('email-placeholder')}`} {...register('email', {
+                        required: `${t('form-required-message')}`,
                         pattern: {
                             value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                            message: 'Inncorect email'
+                            message: `${t('form-incorrect-email-message')}`
                         }
                     })} />
                     <Typography fontSize={14} ml={1} color={theme.palette.error.main}>
@@ -54,11 +57,11 @@ const RegisterForm = () => {
                     </Typography>
                 </Box>
                 <Box sx={sxStylesRegisterForm.boxFoemElement}>
-                    <CustomInput sx={sxStylesRegisterForm.formInputElement} type='password' placeholder='Password' {...register('password', {
-                        required: 'Field is required',
+                    <CustomInput sx={sxStylesRegisterForm.formInputElement} type='password' placeholder={`${t('password-placeholder')}`} {...register('password', {
+                        required: `${t('form-required-message')}`,
                         minLength: {
                             value: 6,
-                            message: 'Password is to short. Min 6 chars'
+                            message: `${t('form-incorrect-password-message')}`
                         },
                         onChange: (e) => { setPasswordValue(e.target.value) }
                     })} />
@@ -67,29 +70,31 @@ const RegisterForm = () => {
                     </Typography>
                 </Box>
                 <Box sx={sxStylesRegisterForm.boxFoemElement}>
-                    <CustomInput sx={sxStylesRegisterForm.formInputElement} type='password' placeholder='Repeat password' {...register('repeatPassword', {
-                        required: 'Field is required',
+                    <CustomInput sx={sxStylesRegisterForm.formInputElement} type='password' placeholder={`${t('repeat-password-placeholder')}`} {...register('repeatPassword', {
+                        required: `${t('form-required-message')}`,
                         onChange: (e) => { setRepeatPasswordValue(e.target.value) }
                     })} />
                     <Typography fontSize={14} ml={1} color={theme.palette.error.main}>
                         {errors.repeatPassword?.message}
-                        {passwordValue !== repeatPasswordValue ? 'The passwords are not similar' : null}
+                    </Typography>
+                    <Typography fontSize={14} ml={1} color={theme.palette.error.main}>
+                        {passwordValue !== repeatPasswordValue ? `${t('form-repeat-password-message')}` : null}
                     </Typography>
                 </Box>
                 <Box sx={sxStylesRegisterForm.checkboxFormElement}>
-                    <FormControlLabel label='Confirm regulations' labelPlacement="start" control={<Checkbox color="primary" {...register('confirmRegulation', {
-                        required: 'Field is required'
+                    <FormControlLabel label={`${t('confirm-regulations-label')}`} labelPlacement="start" control={<Checkbox color="primary" {...register('confirmRegulation', {
+                        required: `${t('form-required-message')}`
                     })} />} />
                     <Typography fontSize={14} ml={1} color={theme.palette.error.main}>
                         {errors.confirmRegulation?.message}
                     </Typography>
                 </Box>
                 <Box sx={sxStylesRegisterForm.boxFoemElement}>
-                    <Button type="submit" sx={sxStylesRegisterForm.formInputElement} variant="contained">Register</Button>
+                    <Button type="submit" sx={sxStylesRegisterForm.formInputElement} variant="contained">{`${t('register-button')}`}</Button>
                 </Box>
             </Box>
             <Box sx={sxStylesRegisterForm.boxFoemElement}>
-                <Button onClick={handleNavigateToLoginForm} type="button" sx={sxStylesRegisterForm.formInputElement} variant="contained">Back to login</Button>
+                <Button onClick={handleNavigateToLoginForm} type="button" sx={sxStylesRegisterForm.formInputElement} variant="contained">{`${t('back-to-login-button')}`}</Button>
             </Box>
         </>
     );
