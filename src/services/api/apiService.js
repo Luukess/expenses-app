d78ccb69b9c5fead7cf479a5255cb2de7aa3ef83
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2023-05-23 17:50:35 +0200

    Feat: installed axios lib, created servise to connect ith api

---
+++ src/services/api/apiService.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:4700//api',
+    timeout: '4000',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+});
+
+export const handleAuthLogin = async (data) => {
+    return axiosInstance.post('/auth/login', data);
+};
+
+export const handleAuthLogout = async (data) => {
+    return axiosInstance.delete('/auth/logout', data);
+};
+
+export const handleAuthRegister = async (data) => {
+    return axiosInstance.post('auth/register', data);
+}; 
