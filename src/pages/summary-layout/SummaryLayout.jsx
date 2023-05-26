3f58e2406ee76dc5889d9d80afcbe2115825742a
Lukasz Chwiedosik <lukasz.chwiedosik@gmail.com>
2023-05-26 20:51:54 +0200

    Feat: created chart template

--- src/pages/summary-layout/SummaryLayout.jsx
+++ src/pages/summary-layout/SummaryLayout.jsx
@@ -1,23 +1,38 @@
 import React from "react";
 import Grid from '@mui/material/Unstable_Grid2';
 import DonutChart from "./components/donut-chart/DonutChart";
+import { Box, Paper, Typography } from "@mui/material";
+import { sxStylesSummaryLayout } from "./summaryLayout.style";
 
 const SummaryLayout = () => {
 
     return (
         <>
-            <Grid container spacing={3}>
-                <Grid xs={4} sx={{ backgroundColor: 'red' }}>
-                    <DonutChart />
-                </Grid>
-                <Grid xs={4} sx={{ backgroundColor: 'pink' }}>
-                    walets
-                </Grid>
-                <Grid xs={4} sx={{ backgroundColor: 'yellow' }}>
-                    budgets
-                </Grid>
+            <Box mt={1}>
+                <Grid container spacing={2}>
+                    <Grid xs={12} sm={12} md={6} lg={4} >
+                        <Paper sx={sxStylesSummaryLayout.gridPaper} elevation={4}>
+                            <Typography variant="h6">
+                                Wydatki według kategorii
+                            </Typography>
+                            <Box sx={sxStylesSummaryLayout.donutBox}>
+                                <DonutChart />
+                            </Box>
+                        </Paper>
+                    </Grid>
+                    <Grid xs={12} sm={12} md={6} lg={4} sx={{ backgroundColor: 'pink' }}>
+                        <Paper>
+                            walets
+                        </Paper>
+                    </Grid>
+                    <Grid xs={12} sm={12} md={12} lg={4} sx={{ backgroundColor: 'yellow' }}>
+                        <Paper>
+                            budgets
+                        </Paper>
+                    </Grid>
 
-            </Grid>
+                </Grid>
+            </Box>
         </>
     );
 };
---
+++ src/pages/summary-layout/summaryLayout.style.js
@@ -0,0 +1,10 @@
+export const sxStylesSummaryLayout = {
+    donutBox: {
+        minHeight: '350px',
+        padding: '10px'
+    },
+    gridPaper: {
+        padding: '10px',
+        borderRadius: '10px'
+    }
+};
